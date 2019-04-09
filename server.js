8f43b6637b07d500b8d07bcb6eb2504dfa3d44a5
Natalie <nataliehe333@hotmail.com>
2019-04-09 18:58:24 +0100

    Update server.js
    Added solution for the Exercise Tracker project.

--- server.js
+++ server.js
@@ -1,23 +1,88 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const cors = require('cors')
+const cors = require('cors');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+// connect to mongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, function(error) {
+  console.log(error);
+});
 
-app.use(cors())
+// set up schema and model for database
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true},
+  exercise: [{
+    description: String,
+    duration: String,
+    date: String
+  }]
+});
+const User = mongoose.model('User', userSchema);
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// create a new user
+app.post('/api/exercise/new-user', function(req, res) {
+  const newUser = new User({username: req.body.username});
+  newUser.save(function(err, user) {
+    console.log(err);
+    console.log(user);
+    res.json({username: user.username, _id: user._id});
+  });
+});
+
+// get an array of all users including their usernames and id
+app.get('/api/exercise/users', function(req, res) {
+  User.find({}).then(function(users) {
+    res.json(users);
+  });
+});
+
+// add exercise to an existing user
+app.post('/api/exercise/add', function(req, res) {
+  const date = req.body.date ? req.body.date : new Date().toISOString().slice(0,10);
+  const newExercise = {description: req.body.description, duration: req.body.duration, date: date};
+  User.findByIdAndUpdate(req.body.userId, {$push: {exercise: newExercise}}, {new: true}).then(function(user) {
+    res.json(user);
+  });
+});
+
+// retrieve user exercise log by querying using userId, and optionally from/to dates and limit number of returns
+// i.e.  /api/exercise/log?{userId}[&from][&to][&limit]
+app.get('/api/exercise/log', function(req, res) {
+  const userId = req.query.userId;
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
+  var log = [];
+  
+  User.findById(userId, function(err, user) {
+    user.exercise.sort((a, b) => {
+      return Date.parse(a.date) - Date.parse(b.date);
+    }).map(exercise => 
+         log.push({date: exercise.date, description: exercise.description, duration: exercise.duration})
+    );
+
+    if (from) { log = log.filter( exercise => { return Date.parse(exercise.date) >= Date.parse(from) })};
+    if (to) { log = log.filter( exercise => { return Date.parse(exercise.date) <= Date.parse(to) })};
+    if (limit<log.length) { log = log.slice(0, limit) };
+    
+    log.push({'total exercise count': log.length});    
+    
+    res.json(log);
+  });
+});
 
 // Not found middleware
 app.use((req, res, next) => {
